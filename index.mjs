d9e6330c321a5a50968a281acaa7be953ca8f06b
Anton Lavrenov <lavrton@gmail.com>
2024-05-23 10:19:06 -0500

    new test

--- index.mjs
+++ index.mjs
@@ -1,23 +1,31 @@
-import { fork } from 'child_process';
-import path from 'path';
+import puppeteer from 'puppeteer';
 
-const scriptPath = path.resolve('./startBrowser.mjs');
+async function createBrowser() {
+  return puppeteer.launch({
+    args: [
+      '--enable-logging=stderr',
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-accelerated-2d-canvas',
+      '--no-first-run',
+      '--no-zygote',
+      '--single-process', // This might increase memory consumption
+      '--disable-gpu',
+    ],
+    headless: 'new',
+  });
+}
 
-async function startProcesses() {
+async function run() {
   for (let i = 0; i < 50; i++) {
-    const child = fork(scriptPath);
-    child.on('message', (msg) => {
-      console.log(`Child ${i} message:`, msg);
-    });
-    child.on('exit', (code) => {
-      console.log(`Child ${i} exited with code ${code}`);
-    });
-    console.log('Started process', i);
+    const browser = await createBrowser();
+    console.log('created browser', i);
     await new Promise((resolve) => setTimeout(resolve, 1000));
   }
 }
 
-startProcesses().catch((e) => {
+run().catch((e) => {
   console.error(e);
   process.exit(1);
 });
